c7a850f2628c3624ebf78c6992cac4725dd5ec88
Kevin-Carbajal-1052 <164533993+Kevin-Carbajal-1052@users.noreply.github.com>
2024-09-06 12:11:02 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,10 @@
+# Ejemplos de conjuntos
+Frutas={"Manzana", "Naranja", "Fresa"}
+print(Frutas)
+print("--Longitud de un conjunto--\n")
+print(len(Frutas))
+Frutas.add("Pera")
+Mascotas={"Perro", "Gato","Pantera"}
+
+for miMascota in Mascotas:
+    print(miMascota)
---
+++ diccionarios.py
@@ -0,0 +1,13 @@
+#Ejemplso de diccionario
+
+Escuela={
+    "ID": "123",
+    "Alumno": "Kevin",
+    "Maestros": "El aldeano",
+    "Materia": "Dis que enseñar python"
+}
+print(Escuela)
+print(Escuela["Alumno"])
+
+for elementos in Escuela:
+    print(elementos)
